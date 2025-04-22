e4382661e7d65e1b8e4b4ca0c5f358cd2ccffcab
ZhaangLi <18072416561@163.com>
2025-04-22 11:26:26 +0800

    项目建立

---
+++ src/main/java/cn/lanqiao/lanqiaojjj_shop_multy/LanqiaoJjjShopMultyApplication.java
@@ -0,0 +1,13 @@
+package cn.lanqiao.lanqiaojjj_shop_multy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LanqiaoJjjShopMultyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LanqiaoJjjShopMultyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cn/lanqiao/lanqiaojjj_shop_multy/LanqiaoJjjShopMultyApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.lanqiao.lanqiaojjj_shop_multy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LanqiaoJjjShopMultyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
